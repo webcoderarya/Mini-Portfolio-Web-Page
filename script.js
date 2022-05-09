f2dad120717a3f2043c716d26308c02f267a5d4b
Dhiraj <dhirajkum00380@gmail.com>
2022-05-09 05:37:23 +0530

    "master"

---
+++ script.js
@@ -0,0 +1,13 @@
+// let btnclick = document.getElementById("projects");
+// let show = document.getElementsByClassName("skills-box");
+
+// btnclick.addEventListener("click",function(){
+//     // console.log("hello")
+//     show[0].classList.add("showskills")
+//     show[0].classList.remove("showskills")
+// })
+
+$('.skills-box').click(function () {
+    $('.skills-box').addcl
+    // $('.skills-box').toggleClass("showskills");
+})
